04044d540155a08846fb358945b76407d77cfa3c
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-08-13 23:07:53 -0400

    first commit

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -209,7 +209,48 @@ var _bro = require("./bro");
 
 require("./styles/main.scss");
 
-document.querySelector("h1").textContent = (0, _bro.bro)("hows it going");
+document.querySelector("h1").textContent = (0, _bro.bro)("hows it going"); // console.log("hi");
+// for (let hash = "#"; hash.length < 8; hash += "#") {
+//   console.log(hash);
+// }
+// for (let num = 0; num <= 100; num++) {
+//   if (num % 3 == 0) {
+//     console.log(num + " FIZZ");
+//   } else if (num % 5 == 0) {
+//     console.log(num + " BUZZ");
+//   } else {
+//     console.log(num + " Neither");
+//   }
+// }
+// for (let n = 1; n <= 100; n++) {
+//   let output = "";
+//   if (n % 3 == 0) output = +"yes";
+//   if (n % 5 == 0) output = +"no";
+//   console.log(output || n);
+// }
+
+for (var i = "#####"; i.length < 8; i += " ") {
+  for (var j = "#####"; j.length < 8; j += " ") {
+    console.log(i + "\n" + " " + j); // console.log(j);
+  }
+}
+
+var size = 8;
+var board = "";
+
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+
+  board += "\n";
+}
+
+console.log(board);
 },{"./bro":"bro.js","./styles/main.scss":"styles/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -2,3 +2,47 @@ import { bro } from "./bro";
 import "./styles/main.scss";
 
 document.querySelector("h1").textContent = bro("hows it going");
+
+// console.log("hi");
+// for (let hash = "#"; hash.length < 8; hash += "#") {
+//   console.log(hash);
+// }
+
+// for (let num = 0; num <= 100; num++) {
+//   if (num % 3 == 0) {
+//     console.log(num + " FIZZ");
+//   } else if (num % 5 == 0) {
+//     console.log(num + " BUZZ");
+//   } else {
+//     console.log(num + " Neither");
+//   }
+// }
+
+// for (let n = 1; n <= 100; n++) {
+//   let output = "";
+//   if (n % 3 == 0) output = +"yes";
+//   if (n % 5 == 0) output = +"no";
+//   console.log(output || n);
+// }
+
+for (let i = "#####"; i.length < 8; i += " ") {
+  for (let j = "#####"; j.length < 8; j += " ") {
+    console.log(i + "\n" + " " + j);
+    // console.log(j);
+  }
+}
+
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+console.log(board);
